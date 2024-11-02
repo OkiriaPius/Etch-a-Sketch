5c681113bbe83cc0b82824de31b80002e81b98d9
OkiriaPius <okiriapiusicloud@gmail.com>
2024-11-03 01:04:40 +0300

    generate equal box size also height and width setting rgb random color and opacity

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const containerDiv = document.querySelector(".container");
 const root = document.documentElement;
+const boxSize = 10;
+root.style.setProperty("--container-width", ``);
 
 let numberOfBox;
 let newBoxNumber;
@@ -9,43 +11,43 @@ promptButton.setAttribute("style", "margin: 5px; padding: 4px; width: 350px; box
 document.body.append(promptButton);
 
 promptButton.addEventListener("click", () => {
-   numberOfBox = parseInt(prompt("Type in the number of boxes:"));
+   numberOfBox = Number(prompt("Type in the number of boxes:"));
    containerDiv.innerHTML = "";
    newBoxNumber = numberOfBox * numberOfBox;
+   if (numberOfBox > 100) {
+    alert("HEY!, NOT BEYOND 100");
+    }else{
+        for (let i = 0; i < (newBoxNumber); i++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("numbered");
+            containerDiv.appendChild(newDiv);
+        }
+    }
    
-   for (let i = 0; i < (newBoxNumber); i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("numbered");
-    containerDiv.appendChild(newDiv);
-   }
 
 
     const newDivs = document.querySelectorAll(".numbered");
     newDivs.forEach( newDiv => {
     newDiv.addEventListener("mouseover", () => {
-        let rgbColor = Math.random() * 225;
+        let rgbColor = Math.round(Math.random() * 225);
         
-        newDiv.setAttribute("style", `background-color: rgb(${rgbColor},${rgbColor},${rgbColor});`);
+        newDiv.setAttribute("style", `background-color: rgb(${rgbColor},${rgbColor},${rgbColor}); opacity: 0.1;`);
+        console.log(`background-color: rgb(${rgbColor},${rgbColor},${rgbColor})`)
     });
-    console.log(numberOfBox-2);     
+    console.log(numberOfBox);     
     });
-
-    if (!isNaN(newBoxNumber) && !isNaN(numberOfBox-2) && newBoxNumber > 0 && numberOfBox > 0) {
-        
-        if(numberOfBox > 22) {
-            const percent = newBoxNumber * (15/100);
-            root.style.setProperty("--box-width", `18px`)
-            root.style.setProperty("--container-width", `${newBoxNumber - percent}px`);
-            console.log(numberOfBox);
-        }else{
-            root.style.setProperty("--box-width", `${numberOfBox-2}px`);
-            root.style.setProperty("--container-width", `${newBoxNumber}px`);
-        
-        
-        }        
+    if (numberOfBox > 100) {
+        root.style.setProperty("--container-width", ``);
+         
+    }else if (!isNaN(newBoxNumber) && !isNaN(numberOfBox-2) && newBoxNumber > 0 && numberOfBox > 0) {
+            
+        console.log(`${numberOfBox * boxSize}px`);
+        root.style.setProperty("--container-width", `${numberOfBox * boxSize}px`);
+         
            
-    }else {
+    } else {
         alert("Please enter valid positive numbers!");
+        console.log("no");
     };
     
 });
