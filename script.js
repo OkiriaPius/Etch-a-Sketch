dce5468e515fff0c0039886c449007c564d23d6c
OkiriaPius <okiriapiusicloud@gmail.com>
2024-11-02 18:48:19 +0300

    adding random color and working on the mouse movement plus button for inputing the number of box

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 const containerDiv = document.querySelector(".container");
-for (let i = 0; i < 256; i++) {
+const root = document.documentElement;
+
+let numberOfBox;
+let newBoxNumber;
+const promptButton = document.createElement("button");
+promptButton.innerText = "How many boxes? Click the button to enter number of boxes";
+promptButton.setAttribute("style", "margin: 5px; padding: 4px; width: 350px; box-shadow: 2px 4px rgba(0,0,0, .3); border-radius: 4px; cursor: pointer; position: fixed; top: 20px;");
+document.body.append(promptButton);
+
+promptButton.addEventListener("click", () => {
+   numberOfBox = parseInt(prompt("Type in the number of boxes:"));
+   containerDiv.innerHTML = "";
+   newBoxNumber = numberOfBox * numberOfBox;
+   
+   for (let i = 0; i < (newBoxNumber); i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("numbered");
-    containerDiv.appendChild(newDiv); 
-}
-const newDivs = document.querySelectorAll(".numbered");
-newDivs.forEach(newDiv => {
+    containerDiv.appendChild(newDiv);
+   }
+
+
+    const newDivs = document.querySelectorAll(".numbered");
+    newDivs.forEach( newDiv => {
     newDiv.addEventListener("mouseover", () => {
-        newDiv.setAttribute("style", "background-color: yellow;")
-    });    
+        let rgbColor = Math.random() * 225;
+        
+        newDiv.setAttribute("style", `background-color: rgb(${rgbColor},${rgbColor},${rgbColor});`);
+    });
+    console.log(numberOfBox-2);     
+    });
+
+    if (!isNaN(newBoxNumber) && !isNaN(numberOfBox-2) && newBoxNumber > 0 && numberOfBox > 0) {
+        
+        if(numberOfBox > 22) {
+            const percent = newBoxNumber * (15/100);
+            root.style.setProperty("--box-width", `18px`)
+            root.style.setProperty("--container-width", `${newBoxNumber - percent}px`);
+            console.log(numberOfBox);
+        }else{
+            root.style.setProperty("--box-width", `${numberOfBox-2}px`);
+            root.style.setProperty("--container-width", `${newBoxNumber}px`);
+        
+        
+        }        
+           
+    }else {
+        alert("Please enter valid positive numbers!");
+    };
+    
 });
